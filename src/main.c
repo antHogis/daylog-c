2822e66c9dc7c162327c055bd889250a1e0e1fa3
Anton Höglund <anton.hoglund@vinka.fi>
2024-02-10 00:51:20 +0200

    style: Use spaces for alignment

--- src/main.c
+++ src/main.c
@@ -25,21 +25,21 @@ void init_date_str_regex(void)
 	int errornumber;
 	PCRE2_SIZE erroroffset;
 	date_str_regex = pcre2_compile(pattern,
-								   PCRE2_ZERO_TERMINATED,
-								   0,
-								   &errornumber,
-								   &erroroffset,
-								   NULL);
+	                               PCRE2_ZERO_TERMINATED,
+	                               0,
+	                               &errornumber,
+	                               &erroroffset,
+	                               NULL);
 
 	if (date_str_regex == NULL)
 	{
 		PCRE2_UCHAR buffer[256];
 		pcre2_get_error_message(errornumber, buffer, sizeof(buffer));
 		fprintf(stderr,
-				"FATAL: PCRE2 compilation failed for date_str_regex pattern at offset "
-				"%d: %s\n",
-				(int)erroroffset,
-				buffer);
+		        "FATAL: PCRE2 compilation failed for date_str_regex pattern at offset "
+		        "%d: %s\n",
+		        (int)erroroffset,
+		        buffer);
 		exit(1);
 	}
 }
@@ -56,12 +56,12 @@ int validate_date_str(char* date_str)
 		pcre2_match_data_create_from_pattern(date_str_regex, NULL);
 
 	int rc = pcre2_match(date_str_regex,
-						 (PCRE2_SPTR)date_str,
-						 strlen(date_str),
-						 0,
-						 0,
-						 match_data,
-						 NULL);
+	                     (PCRE2_SPTR)date_str,
+	                     strlen(date_str),
+	                     0,
+	                     0,
+	                     match_data,
+	                     NULL);
 
 	pcre2_match_data_free(match_data);
 
@@ -82,11 +82,11 @@ bool validate_args(Arguments* args)
 		return false;
 	}
 	if ((args->use_csv || args->from_date != NULL || args->to_date != NULL) &&
-		!(args->use_csv && args->from_date != NULL && args->to_date != NULL))
+	    !(args->use_csv && args->from_date != NULL && args->to_date != NULL))
 	{
 		fprintf(stderr,
-				"ERROR: If any of use_csv, from_date and to_date are defined, all must "
-				"be defined\n");
+		        "ERROR: If any of use_csv, from_date and to_date are defined, all must "
+		        "be defined\n");
 		return false;
 	}
 	if (args->from_date != NULL && !validate_date_str(args->from_date))
@@ -106,18 +106,18 @@ bool validate_args(Arguments* args)
 // Get todays date as an YYYY-MM-DD string
 char* get_today_date(void)
 {
-	time_t t	 = time(NULL);
+	time_t t     = time(NULL);
 	struct tm tm = *localtime(&t);
 
 	// need to have a larger buffer size than needed to avoid format-truncation warning.
-	int buffer_size	  = 36;
+	int buffer_size   = 36;
 	char* date_string = malloc(sizeof(char) * buffer_size);
 	snprintf(date_string,
-			 buffer_size,
-			 "%4d-%02d-%02d",
-			 tm.tm_year + 1900,
-			 tm.tm_mon + 1,
-			 tm.tm_mday);
+	         buffer_size,
+	         "%4d-%02d-%02d",
+	         tm.tm_year + 1900,
+	         tm.tm_mon + 1,
+	         tm.tm_mday);
 
 	return date_string;
 }
@@ -127,11 +127,11 @@ int main(int argc, char** argv)
 	Arguments arguments;
 
 	// Default values.
-	arguments.date_arg	= get_today_date();
-	arguments.verbose	= false;
-	arguments.use_csv	= false;
+	arguments.date_arg  = get_today_date();
+	arguments.verbose   = false;
+	arguments.use_csv   = false;
 	arguments.from_date = NULL;
-	arguments.to_date	= NULL;
+	arguments.to_date   = NULL;
 
 	parse_args(argc, argv, &arguments);
 	// printf("[ARGS]\nDate: %s\nUse CSV: %d\nFrom: %s\nTo: %s\n",
